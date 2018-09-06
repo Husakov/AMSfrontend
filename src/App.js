9e03f45ecca27b1e7557a370d1380590dfbe4b39
Samir Husak <samhusak@hotmail.com>
2018-09-06 20:22:02 +0200

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
 class App extends Component {
+  state={
+    isOpen:true,
+
+  };
+  toggle=true;
   render() {
     return (
-      <div className="App">
+      <div>
+          <Navbar  className="navbar1"  expand="md" >
+              <NavbarBrand href="/">AMS Engineering</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
 
+                  <Nav className="ml-auto" navbar>
+
+                        <NavItem>
+                          <NavLink href="">Home</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="">About</NavLink>
+                      </NavItem>
+                      <NavItem>
+                          <NavLink href="">Contact</NavLink>
+                      </NavItem>
+
+                  </Nav>
+              </Collapse>
+          </Navbar>
       </div>
     );
   }
